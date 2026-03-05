24f332bf2a3413085f289ab765ae183449925d53
Drew <akapnick5@gmail.com>
2026-03-05 15:02:36 -0500

    Can delete HM moves

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -2773,12 +2773,7 @@ static void Task_HandleReplaceMoveInput(u8 taskId)
 
 static bool8 CanReplaceMove(void)
 {
-    if (sMonSummaryScreen->firstMoveIndex == MAX_MON_MOVES
-        || sMonSummaryScreen->newMove == MOVE_NONE
-        || !CannotForgetMove(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]))
-        return TRUE;
-    else
-        return FALSE;
+    return TRUE;
 }
 
 static void ShowCantForgetHMsWindow(u8 taskId)
